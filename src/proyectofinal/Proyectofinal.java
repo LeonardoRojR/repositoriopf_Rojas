1429a705a02b5abc007a0529df99991c57ed48f5
LEONARDO ROJAS <LEONARDO ROJAS@DESKTOP-M5C1M8A>
2025-01-21 22:41:04 -0500

    carga inicial

--- src/proyectofinal/Proyectofinal.java
+++ src/proyectofinal/Proyectofinal.java
@@ -5,31 +5,26 @@ import java.util.Scanner;
 public class Proyectofinal {
     public static void main(String args[]) {
         // declarar variables
-        int vt1, vt2, vt3;
+        int vt1, vt2, vt3,vt4;
         double sm, pt, c;
         String vendedor;
         Scanner lectura = new Scanner(System.in);
-        
         // Entrada de datos
         System.out.print("Nombre del vendedor: ");
         vendedor = lectura.nextLine(); 
-        
         System.out.print("Sueldo mensual: ");
         sm = lectura.nextInt();
-        
         System.out.print("Venta 1: ");
         vt1 = lectura.nextInt();
-        
         System.out.print("Venta 2: ");
         vt2 = lectura.nextInt();
-        
         System.out.print("Venta 3: ");
         vt3 = lectura.nextInt();
-        
+       System.out.print("Venta 4: ");
+        vt4 = lectura.nextInt();
         // procesar datos
-        c = (vt1 + vt2 + vt3) * 0.10;
+        c = (vt1 + vt2 + vt3+vt4) * 0.10;
         pt = sm + c;
-        
         // salida de datos
         System.out.println("La comision de la venta es: " + c);
         System.out.println("El sueldo mensual del vendedor es: " + pt);
